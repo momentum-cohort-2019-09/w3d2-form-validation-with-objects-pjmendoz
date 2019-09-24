5deedd4036430abe3b21a18fd2cf12d75d9c4a21
pjmendoz <pearl.j.mendoza@gmail.com>
2019-09-24 14:21:13 -0400

    validating name and days

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log('Add validation!')
+function q (selector){
+    return document.querySelector(selector)
+}
+
+function qs(selector){
+    return document.querySelectorAll(selector)
+}
+
+let field = q('#field')
+let input = q('#input')
+
+class Field {
+    constructor(input) {
+        this.input = input;
+    }
+    validateField() {
+    return this.input !== "";
+    }
+    isNumber() {
+    return !isNaN(this.input)
+    }
+}
+
+q('#parking-form').addEventListener('submit', function(event) {
+    event.preventDefault()
+
+let nameValue = q("#name").value
+
+let nameField= new Field (nameValue)
+
+let daysValue= q("#days").value
+
+let daysField = new Field (daysValue)
+
+nameField.validateField()
+
+console.log(daysField.validateField())
+console.log(daysField.isNumber())
+
+})
+
+
+
+
